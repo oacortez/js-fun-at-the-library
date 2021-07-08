037beeb31bb8a06216f3a87e0c35a640391813ed
Oscar Cortez <oacortez98@gmail.com>
2021-07-08 08:56:25 -0600

    Completed shelfBook section

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,15 @@
+function shelfBook(book, shelf) {
+  if(shelf.length < 3 ) {
+  //   console.log(shelf.push(book))
+    return shelf.unshift(book)
+  // } else {
+  //   return shelf.push(book)
+  // }
+  }
+}
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
