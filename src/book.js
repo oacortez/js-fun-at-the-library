1b65276c3d940c2d413dac181af0f4abb0592ec9
Oscar Cortez <oacortez98@gmail.com>
2021-07-07 18:49:02 -0600

    Completed writeBook test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
 
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name : name,
+    age : age,
+    pronouns : pronouns
+  }
+  return character
+}
+
+
+function saveReview(bookTitle,reviews) {
+  if(reviews.length === 0){
+    return reviews.push(bookTitle)
+  }
+  for (var i = 0; i < reviews.length; i++) {
+    if (bookTitle === reviews[i]) {
+      return reviews;
+    } else {
+      reviews.push(bookTitle);
+      return reviews;
+    }
+  }
+}
+
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.split('').length * 20;
+  return bookPageCount;
+}
+
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(bookTitle)
+  }
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
