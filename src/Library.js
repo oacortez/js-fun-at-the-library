77fb2a952a62cd75c7a77dc0b6f1c973330e7c82
Oscar Cortez <oacortez98@gmail.com>
2021-07-10 09:20:46 -0600

    Completed Iteration 3

--- src/Library.js
+++ src/Library.js
@@ -14,8 +14,27 @@ function createLibrary(name) {
 function addBook(libraryName, bookName) {
   libraryName.shelves[bookName.genre].push(bookName);
 }
+
+
+function checkoutBook(library, bookName, genre) {
+  // maybe create a for loop to get in the object of the property shelves and take out book from
+  //  its genre. inside the for loop will create a something that has interpolatiom
+  // saying that it took that book from the library
+
+  var shelf = library.shelves[genre]
+
+  for(var i = 0; i < shelf.length; i++) {
+    var book = shelf[i]
+    if(bookName === book.title) {
+      shelf.splice(i, 1);
+      return `You have now checked out ${bookName} from the ${library.name}`
+
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
+}
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
