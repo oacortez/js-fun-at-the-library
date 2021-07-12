6a7235d06e6148d018ddfd7c32686fdf31202d2b
Oscar Cortez <oacortez98@gmail.com>
2021-07-11 22:06:11 -0600

    Completed extra pratice

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,26 @@
 class Librarian {
-  constructor() {
-    
+  constructor(personName, library) {
+    this.name = personName;
+    this.library = library
   }
-}
+  greetPatron(name, morning) {
+    if(morning) {
+      return `Good morning, ${name}!`
+    }
+    return `Hello, ${name}!`
+  }
+  findBook(book) {
+    for(var i = 0; i < this.library.shelves.fantasy.length; i++) {
+        if(this.library.shelves.fantasy[i].title === book) {
+          this.library.shelves.fantasy.splice(i, 1);
+          return `Yes, we have ${book}`;
+        }
+      }
+      return `Sorry, we do not have ${book}`;
+    }
+    calculateLateFee(days) {
+      return Math.ceil(days * 0.25)
+    }
+  }
 
 module.exports = Librarian;
